3badb42842a2d37f83764cb0e05f5d12df86fb0c
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-08-18 21:36:41 +0530

    day-14
    setup the welcome mail function

--- floak/settings.py
+++ floak/settings.py
@@ -148,6 +148,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'developer.udithsandaruwan@gmail.com'
+EMAIL_HOST_PASSWORD = 'nyth pvka xmoy jyqq'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
+from django.core.mail import send_mail
+from django.conf import settings
 
 def createProfile(sender, instance, created, **kwargs):
     if created:
@@ -10,6 +12,17 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username, 
             email=user.email,
             name=user.first_name, 
+        )
+        
+        subject = 'Welcome To Million Passes'
+        message = 'We are glade to see you at our platform.'
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 def updateUser(sender, instance, created, **kwargs):
