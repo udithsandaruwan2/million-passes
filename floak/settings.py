f1ca1b66ad15c32b5c6c752e92fb25a9d78e877b
Udith Sandaruwan <168072320+udithsandaruwan2@users.noreply.github.com>
2025-01-23 18:04:31 +0530

    Update settings.py

--- floak/settings.py
+++ floak/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6b9q%^!d0(*mjl4+=21tcnhr2pqb_s+z9iw&v*!4j#7f-3$le'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'developer.udithsandaruwan@gmail.com'
-EMAIL_HOST_PASSWORD = 'nyth pvka xmoy jyqq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Static files (CSS, JavaScript, Images)
